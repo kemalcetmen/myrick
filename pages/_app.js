03463015b4ef33bc72effab3f3f269bc377398ae
Kemal Cetmen <kemalcetmensm@gmail.com>
2022-04-13 23:40:21 +0300

    screen size

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ export default function  myapp({Component, pageProps}){
         <>
          <Component {...pageProps}/>
          <Head>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
           <title>My Rick</title>
           <link rel="shortcut icon" href="/images/favicon.ico" />
           <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
